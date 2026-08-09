3d263cddca1b5902f734d9a87204d85b71868c43
lalu4321 <lalkrishna4lsg@gmail.com>
2026-08-09 19:03:25 +0530

    Completed common utilities and dashboard

--- backend/common/permissions.py
+++ backend/common/permissions.py
@@ -1,27 +1,44 @@
 from rest_framework.permissions import BasePermission
 
 
+# ==========================================================
+# Admin Permission
+# ==========================================================
+
 class IsAdmin(BasePermission):
 
     def has_permission(self, request, view):
+
         return (
             request.user.is_authenticated
             and request.user.role == "ADMIN"
         )
 
 
+
+# ==========================================================
+# Seller Permission
+# ==========================================================
+
 class IsSeller(BasePermission):
 
     def has_permission(self, request, view):
+
         return (
             request.user.is_authenticated
             and request.user.role == "SELLER"
         )
 
 
+
+# ==========================================================
+# Customer Permission
+# ==========================================================
+
 class IsCustomer(BasePermission):
 
     def has_permission(self, request, view):
+
         return (
             request.user.is_authenticated
             and request.user.role == "CUSTOMER"
